6b0fa3d2f7d4846c2bf3ce3d1c53fda5daf29070
Arsen <Arsen@Arsen-PC>
2014-12-12 11:29:39 +0400

    1. Improvements to "Mail" module

--- System/Modules/Mail.php
+++ System/Modules/Mail.php
@@ -22,7 +22,7 @@ class Mail extends \Module {
     private $charset = 'utf-8';
     private $contentType = 'text/html';
     private $mimeVersion = '1.0';
-    
+
     public function __construct() {
         parent::__construct();
 
@@ -35,42 +35,64 @@ class Mail extends \Module {
             $this->charset = $charset;
         }
     }
-    
+
     public function contentType($contentType = '') {
 
-        if (!empty($contentType)) {
+        if (!empty(trim($contentType))) {
             $this->contentType = $contentType;
         }
     }
-    
+
     public function mimeVersion($mimeVersion = '') {
 
-        if (!empty($mimeVersion)) {
+        if (!empty(trim($mimeVersion))) {
             $this->mimeVersion = $mimeVersion;
         }
     }
-    
-    public function to($to = []) {
-                
-        $to = is_array($to) ? $to : [$to];
-        $this->to = array_unique(array_merge($this->to, $to));
+
+    public function headers($headers = [], $merge = true) {
+
+        if ($merge) {
+            $headers = is_array($headers) ? $headers : [$headers];
+            $this->headers = array_unique(array_merge($this->headers, $headers));
+        } else {
+            $this->headers = is_array($headers) ? $headers : [$headers];
+        }
     }
 
-    public function cc($cc = []) {
+    public function from($from = '') {
+
+        $this->from = $from;
+    }
+
+    public function to($to = [], $merge = true) {
         
-        $cc = is_array($cc) ? $cc : [$cc];
-        $this->cc = array_unique(array_merge($this->cc, $cc));
+        if ($merge) {
+            $to = is_array($to) ? $to : [$to];
+            $this->to = array_unique(array_merge($this->to, $to));
+        } else {
+            $this->to = is_array($to) ? $to : [$to];
+        }
     }
 
-    public function bcc($bcc = []) {
+    public function cc($cc = [], $merge = true) {
         
-        $bcc = is_array($bcc) ? $bcc : [$bcc];
-        $this->bcc = array_unique(array_merge($this->bcc, $bcc));
+        if ($merge) {
+            $cc = is_array($cc) ? $cc : [$cc];
+            $this->cc = array_unique(array_merge($this->cc, $cc));
+        } else {
+            $this->cc = is_array($cc) ? $cc : [$cc];
+        }
     }
 
-    public function message($message = '') {
-
-        $this->message = $message;
+    public function bcc($bcc = [], $merge = true) {
+        
+        if ($merge) {
+            $bcc = is_array($bcc) ? $bcc : [$bcc];
+            $this->bcc = array_unique(array_merge($this->bcc, $bcc));
+        } else {
+            $this->bcc = is_array($bcc) ? $bcc : [$bcc];
+        }
     }
 
     public function subject($subject = '') {
@@ -78,34 +100,26 @@ class Mail extends \Module {
         $this->subject = $subject;
     }
 
-    public function from($from = '') {
+    public function message($message = '') {
 
-        if (!empty($from)) {
-            $this->from = $from;
-        }
+        $this->message = $message;
     }
-    
-    public function headers($headers = []) {
 
-        $headers = is_array($headers) ? $headers : [$headers];
-        $this->headers = array_unique(array_merge($this->headers, $headers));
-    }
-    
     public function send() {
 
         $to = implode(',', $this->to);
         $cc = implode(',', $this->cc);
         $bcc = implode(',', $this->bcc);
-        
+
         $headers = 'MIME-Version: ' . $this->mimeVersion . "\r\n";
         $headers.= 'Content-type: ' . $this->contentType . '; charset=' . $this->charset . "\r\n";
-        $headers.= 'To: ' . $to . "\r\n";
-        $headers.= 'From: ' . $this->from . "\r\n";
-        $headers.= 'Cc: ' . $cc . "\r\n";
-        $headers .= 'Bcc: ' . $bcc . "\r\n";
+        $headers.= empty(trim($to)) ? '' : 'To: ' . $to . "\r\n";
+        $headers.= empty(trim($this->from)) ? '' : 'From: ' . $this->from . "\r\n";
+        $headers.= empty(trim($cc)) ? '' : 'Cc: ' . $cc . "\r\n";
+        $headers .= empty(trim($bcc)) ? '' : 'Bcc: ' . $bcc . "\r\n";
         $headers.= implode("\r\n", $this->headers);
         
         return mail($to, $this->subject, $this->message, $headers);
     }
-}
 
+}
