af2b06de02ceb28fdf8e3d5f3149047ddb94f25e
Arsen <Arsen@192.168.0.94>
2015-06-02 21:51:28 +0400

    1. Fix to previous commit

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,16 +3,6 @@
 class IndexController extends Controller {
 
     public function indexAction($data) {
-        $this->module('cache');
-        $this->module('image');
-        $this->module('image');
-        $this->module('language');
-        $this->module('mail');
-        $this->module('validator');
-        $this->core('config');
-        $this->core('session');
-        $this->core('uri');
-        //$this->core('db');
 
         $this->view('', ['hello' => 'Hello World!']);
     }
